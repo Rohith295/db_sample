6aee22918702a8f58024da629a562ed0701a94a0
Rohith <rangaraju963@gmail.com>
2021-10-21 21:39:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
+from setuptools import setup
+from wheel_proj import __version__
 
+with open("requirements.txt") as f:
+    requirements = f.read().split()
+
+setup(name='wheel_proj',
+      version=__version__,
+      description='Python based test project',
+      author='noone',
+      author_email='noone',
+
+      packages=['wheel_proj'],
+      install_requires=requirements
+      )
